6f059d174ad3a55e636c6142711a2a294e65bc82
Space Pocket <space1985pocket@gmail.com>
2024-02-27 12:28:30 +0300

    feat: add solution for сoding-meetup1_sort-by-lang

---
+++ 7kyu/сoding-meetup1_sort-by-lang.js
@@ -0,0 +1,32 @@
+// DESCRIPTION:
+
+// You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Given the following input array:
+
+// var list1 = [
+//   { firstName: 'Nikau', lastName: 'R.', country: 'New Zealand', continent: 'Oceania', age: 39, language: 'Ruby' },
+//   { firstName: 'Precious', lastName: 'G.', country: 'South Africa', continent: 'Africa', age: 22, language: 'JavaScript' },
+//   { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 30, language: 'C' },
+//   { firstName: 'Agustin', lastName: 'V.', country: 'Uruguay', continent: 'Americas', age: 19, language: 'JavaScript' }
+// ];
+// Write a function that returns the array sorted alphabetically by the programming language. In case there are some developers that code in the same language, sort them alphabetically by the first name:
+
+// [
+//   { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 30, language: 'C' },
+//   { firstName: 'Agustin', lastName: 'V.', country: 'Uruguay', continent: 'Americas', age: 19, language: 'JavaScript' },
+//   { firstName: 'Precious', lastName: 'G.', country: 'South Africa', continent: 'Africa', age: 22, language: 'JavaScript' },
+//   { firstName: 'Nikau', lastName: 'R.', country: 'New Zealand', continent: 'Oceania', age: 39, language: 'Ruby' }
+// ];
+
+// SOLUTION:
+
+function sortByLanguage(list) {
+  return list.sort((a, b) => {
+    if (a.language === b.language) {
+      return a.firstName > b.firstName ? 1 : -1;
+    }
+    return a.language > b.language ? 1 : -1;
+  });
+}
+
